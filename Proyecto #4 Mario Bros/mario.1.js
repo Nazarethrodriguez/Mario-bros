2ae8a1e25bb4fd684d9540e6df280b8de0658e6e
Nazarethrodriguez <nazarethrodriguez957@gmail.com>
2025-06-28 09:36:22 -0400

    Delete Proyecto #4 Mario Bros directory

--- Proyecto #4 Mario Bros/mario.1.js
+++
@@ -1,2 +0,0 @@
-export let mario = document.querySelector("#mario"); // Asegúrate de tener un elemento con el ID "mario" en tu HTML
-
--- Proyecto #4 Mario Bros/mario.js
+++
@@ -1,3 +0,0 @@
-// ================== Estado del Juego ==================
-// Variables que controlan el movimiento y acciones de Mario
-export const mario = document.getElementById('mario');
